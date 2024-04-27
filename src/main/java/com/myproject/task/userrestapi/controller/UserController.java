7cf0976022c8e3a40907088b5c75752a5368b7ea
voityvaiti <flashlight@i.ua>
2024-04-28 01:10:48 +0300

    UserController update. Api-prefix value.

--- src/main/java/com/myproject/task/userrestapi/controller/UserController.java
+++ src/main/java/com/myproject/task/userrestapi/controller/UserController.java
@@ -1,27 +1,32 @@
 package com.myproject.task.userrestapi.controller;
 
+import com.myproject.task.userrestapi.dto.request.DateRangeReqDto;
+import com.myproject.task.userrestapi.dto.request.UserFieldsUpdateReqDto;
+import com.myproject.task.userrestapi.dto.request.UserCreateReqDto;
+import com.myproject.task.userrestapi.dto.request.UserUpdateReqDto;
+import com.myproject.task.userrestapi.dto.response.UserResDto;
 import com.myproject.task.userrestapi.entity.User;
 
+import com.myproject.task.userrestapi.mapper.UserMapper;
 import com.myproject.task.userrestapi.service.abstraction.UserService;
 import jakarta.validation.Valid;
-import jakarta.validation.ValidationException;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("${api.prefix}/users")
 public class UserController {
 
     private final UserService userService;
+
+    private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
+
 
     @Autowired
     public UserController(UserService userService) {
@@ -29,66 +34,50 @@ public class UserController {
     }
 
 
-    @GetMapping("/birthdate/{fromDate}/{toDate}")
-    public ResponseEntity<List<User>> searchUsersByBirthDateRange(
-            @PathVariable @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate fromDate,
-            @PathVariable @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate toDate) {
+    @GetMapping("/birthdate")
+    public ResponseEntity<List<UserResDto>> getUsersByBirthDateRange(@Valid DateRangeReqDto dateRangeReqDto) {
 
-        List<User> users = userService.getUsersByBirthDateRange(fromDate, toDate);
+        List<User> users = userService.getUsersByBirthDateRange(
+                dateRangeReqDto.getFrom(), dateRangeReqDto.getTo()
+        );
 
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(
+                users.stream().map(userMapper::mapToUserResDto).toList()
+        );
     }
 
-
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody @Valid User user,
-                                       BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            String errorMessage = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                    .collect(Collectors.joining("; "));
+    public ResponseEntity<UserResDto> create(@RequestBody @Valid UserCreateReqDto userCreateReqDto) {
 
-            throw new ValidationException(errorMessage);
-        }
+        User createdUser = userService.create(userMapper.mapToUser(userCreateReqDto));
 
-        User createdUser = userService.add(user);
-
-        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        return new ResponseEntity<>(userMapper.mapToUserResDto(createdUser), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateAllFields(
-            @PathVariable Long id,
-            @RequestBody @Valid User updatedUser,
-            BindingResult bindingResult) {
+    public ResponseEntity<UserResDto> update(@PathVariable Long id, @RequestBody @Valid UserUpdateReqDto userUpdateReqDto) {
 
-        if (bindingResult.hasErrors()) {
-            String errorMessage = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                    .collect(Collectors.joining("; "));
+        User updatedUser = userMapper.mapToUser(userUpdateReqDto);
+        updatedUser = userService.update(id, updatedUser);
 
-            throw new ValidationException(errorMessage);
-        }
-
-        updatedUser = userService.updateAllFields(id, updatedUser);
-
-        return ResponseEntity.ok(updatedUser);
+        return ResponseEntity.ok(
+                userMapper.mapToUserResDto(updatedUser)
+        );
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<User> updateSomeFields(
-            @PathVariable Long id,
-            @RequestBody User updatedUser) {
+    public ResponseEntity<UserResDto> patch(@PathVariable Long id, @RequestBody @Valid UserFieldsUpdateReqDto fieldsUpdateReq) {
 
-        updatedUser = userService.updateSomeFields(id, updatedUser);
+        User userWithUpdatedFields = userMapper.mapToUser(fieldsUpdateReq);
+        User updatedUser = userService.updatePresentFields(id, userWithUpdatedFields);
 
-        return ResponseEntity.ok(updatedUser);
+        return ResponseEntity.ok(
+                userMapper.mapToUserResDto(updatedUser)
+        );
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<User> delete(@PathVariable("id") Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         userService.delete(id);
 
         return ResponseEntity.ok().build();
