11524af265e3ff7ffaf4bc8aa0e31e96f1630920
voityvaiti <flashlight@i.ua>
2024-04-27 11:49:13 +0300

    BirthDateConstraints @Past constraint added

--- src/main/java/com/myproject/task/userrestapi/validation/annotation/user/BirthDateConstraints.java
+++ src/main/java/com/myproject/task/userrestapi/validation/annotation/user/BirthDateConstraints.java
@@ -3,6 +3,7 @@ package com.myproject.task.userrestapi.validation.annotation.user;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,6 +12,8 @@ import java.lang.annotation.Target;
 
 
 @NotNull(message = "Birth date is required.")
+@Past(message = "Birth date must be in the past.")
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = {})
@@ -21,4 +24,5 @@ public @interface BirthDateConstraints {
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
+
 }
